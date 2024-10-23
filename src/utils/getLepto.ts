6907d11215d9d3a6d62512354ea6cc418170df52
Sinval Luz <contato.sinvalluz@gmail.com>
2024-10-23 10:13:30 -0300

    fix: Muda o diretorio na requisição da api
    Esse commit manipula de forma diferente o url da api para requisição funcionar em produção

--- src/utils/getLepto.ts
+++ src/utils/getLepto.ts
@@ -2,6 +2,6 @@ import { FeatureCollection } from '@/types/leptospirose';
 import axios from 'axios';
 
 export async function getLepto(): Promise<FeatureCollection> {
-	const { data } = await axios.get<FeatureCollection>('http://localhost:3000/api/cordenadas');
+	const { data } = await axios.get<FeatureCollection>('/api/cordenadas');
 	return data;
 }
