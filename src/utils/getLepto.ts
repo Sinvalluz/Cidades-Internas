17347fa54a0135eb3fd699766f4296be74692d5e
Sinval Luz <contato.sinvalluz@gmail.com>
2024-10-25 08:51:09 -0300

    fix: Mudando nome da api para getLeptoData

--- src/utils/getLepto.ts
+++ src/utils/getLepto.ts
@@ -2,6 +2,6 @@ import { FeatureCollection } from '@/types/leptospirose';
 import axios from 'axios';
 
 export async function getLepto(): Promise<FeatureCollection> {
-	const { data } = await axios.get<FeatureCollection>('/api/cordenadas');
+	const { data } = await axios.get<FeatureCollection>('/api/getLeptoData');
 	return data;
 }
